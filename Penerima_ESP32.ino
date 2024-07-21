70d3317ee77bb8ab9dce0ceb3011168c0fd73abf
Uriaaaa <126378661+Uriadutu@users.noreply.github.com>
2024-07-21 11:49:11 +0900

    Update Penerima_ESP32.ino

--- Penerima_ESP32.ino
+++ Penerima_ESP32.ino
@@ -11,4 +11,14 @@ void loop() {
     Serial.print("Nilai Yang Dikirimkan: "); // Tampilkan pesan
     Serial.println(sensorValue); // Tampilkan nilai potensiometer
   }
+  baru()
+}
+
+
+void baru() {
+  if (Serial.available() > 0) { // Jika ada data yang tersedia di Serial
+    int sensorValue = Serial.parseInt(); // Baca nilai yang dikirim melalui Serial
+    Serial.print("Nilai Yang Dikirimkan: "); // Tampilkan pesan
+    Serial.println(sensorValue); // Tampilkan nilai potensiometer
+  }
 }
